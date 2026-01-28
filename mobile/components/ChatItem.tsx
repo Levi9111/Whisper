a961526a6323fb83adabf553055b86f6b50c1924
levi9111 <captainlevi9111@gmail.com>
2026-01-28 19:45:56 +0600

    feat: Implement chat functionality with loading and error states, add ChatItem and EmptyUI components

---
+++ mobile/components/ChatItem.tsx
@@ -0,0 +1,81 @@
+import { formatDistanceToNow } from 'date-fns';
+
+import { Pressable, Text, View } from 'react-native';
+
+import { Image } from 'expo-image';
+
+import { useSocketStore } from '@/lib/socket';
+import { Chat } from '@/types';
+
+const ChatItem = ({ chat, onPress }: { chat: Chat; onPress: () => void }) => {
+  const { participant } = chat;
+  const { onlineUsers, typingUsers, unreadChats } = useSocketStore();
+
+  const isOnline = true;
+  const isTyping = false;
+  const hasUnread = false;
+
+  return (
+    <Pressable
+      className='flex-row items-center py-3 active:opacity-70'
+      onPress={onPress}
+    >
+      <View className='relative'>
+        <Image
+          source={{
+            uri: participant.avatar,
+          }}
+          style={{
+            width: 56,
+            height: 56,
+            borderRadius: 999,
+          }}
+        />
+
+        {isOnline && (
+          <View className='absolute bottom-0 right-0 size-4 bggreen-500 rounded-full border-[3px] border-surface ' />
+        )}
+      </View>
+
+      {/* chat info */}
+
+      <View className='flex-1 ml-4'>
+        <View className='flex-row items-center justify-center'>
+          <Text
+            className={`text-base font-medium ${hasUnread ? 'text-primary' : 'text-foreground'}`}
+          >
+            {participant.name}
+          </Text>
+
+          <View className='flex-row items-center gap-2'>
+            {hasUnread && (
+              <view className='w-2.5 h-2.5 bg-primary rounded-full' />
+            )}
+
+            <Text className='text-xs text-subtle-foreground'>
+              {chat.lastMessageAt
+                ? formatDistanceToNow(new Date(chat.lastMessageAt))
+                : ''}
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View className='flex-row items-center justify-between mt-1'>
+        {isTyping ? (
+          <Text className='text-sm text-primary italic'>typing</Text>
+        ) : (
+          <Text
+            className={`
+            text-sm flex-1 mr-3 ${hasUnread ? 'text-foreground font-medium' : 'text-subtle-foreground'}
+            `}
+          >
+            {chat.lastMessage?.text || 'No message yet'}
+          </Text>
+        )}
+      </View>
+    </Pressable>
+  );
+};
+
+export default ChatItem;
